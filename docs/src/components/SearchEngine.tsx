15db4e64fda1dacdfef062e192b7bad89eb35c97
mrdannael <mr.dannael@gmail.com>
2023-11-19 12:48:50 +0100

    Improve docs SearchEngine with clear button for input

--- docs/src/components/SearchEngine.tsx
+++ docs/src/components/SearchEngine.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Fuse from 'fuse.js';
 import SVG from "react-inlinesvg";
 import { ToastContainer, toast } from "react-toastify";
@@ -61,23 +61,29 @@ const Results = ({ searchResults }: { searchResults: Icon[] }) => {
 }
 
 export default function SearchEngine() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [search, setSearch] = useState<string>('');
   const [searchResults, setSearchResults] = useState<Icon[]>(IconsSet.icons);
 
-  const handleSearch = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    const { value } = event.currentTarget;
-
-    // If the user searched for an empty string,
-    // display all data.
-    if (value.length === 0) {
+  useEffect(() => {
+    if (search.length === 0) {
       setSearchResults(IconsSet.icons);
       return;
     }
 
-    const results = fuse.search(value);
+    const results = fuse.search(search);
     const items = results.map((result) => result.item);
     setSearchResults(items);
+  }, [search])
+
+  const handleSearch = (event: React.FormEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setSearch(event.currentTarget.value);
   };
+
+  const handleClear = () => {
+    setSearch('');
+  }
 
   return (
     <div>
@@ -87,7 +93,14 @@ export default function SearchEngine() {
             <path fill="currentColor" d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5l-1.5 1.5l-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16A6.5 6.5 0 0 1 3 9.5A6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14S14 12 14 9.5S12 5 9.5 5Z" />
           </svg>
         </div>
-        <input type="text" id="default-search" className="outline-none block w-full p-4 ps-10 text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-coolrose focus:border-coolrose font-semibold" placeholder="Search icons..." required onChange={handleSearch} />
+        <input value={search} type="text" id="default-search" className="outline-none block w-full p-4 ps-10 text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-coolrose focus:border-coolrose font-semibold" placeholder="Search icons..." required onChange={handleSearch} />
+        {
+          search.length ?
+            <button type="button" className="text-coolnavy absolute top-4 right-4 hover:text-coolred focus:outline-none" onClick={handleClear}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2m-3.4 14L12 13.4L8.4 17L7 15.6l3.6-3.6L7 8.4L8.4 7l3.6 3.6L15.6 7L17 8.4L13.4 12l3.6 3.6l-1.4 1.4Z"/></svg>
+            </button>
+          : null
+        }
       </div>
       <Results searchResults={searchResults} />
       <ToastContainer progressStyle={{ background: "#f53d7a"}} />
